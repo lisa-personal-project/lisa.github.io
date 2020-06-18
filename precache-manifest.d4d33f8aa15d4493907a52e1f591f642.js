fb47d35cba46334e74b90361dc88e5a00503660b
lisa-personal-project <67097179+lisa-personal-project@users.noreply.github.com>
2020-06-18 13:04:25 +0200

    Updates

---
+++ precache-manifest.d4d33f8aa15d4493907a52e1f591f642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e939236ec0a13bff9e55b95e2b9271",
+    "url": "/project-presentation/index.html"
+  },
+  {
+    "revision": "b4373ba837d3968cadd5",
+    "url": "/project-presentation/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "8b0df438e78d4bcb4fdc",
+    "url": "/project-presentation/static/js/2.c469a002.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-presentation/static/js/2.c469a002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4373ba837d3968cadd5",
+    "url": "/project-presentation/static/js/main.6749d095.chunk.js"
+  },
+  {
+    "revision": "bfa3cb8df49d7b3e275e",
+    "url": "/project-presentation/static/js/runtime-main.7609aa61.js"
+  },
+  {
+    "revision": "a945d858914aa9a7e5b51b8eea7f2771",
+    "url": "/project-presentation/static/media/raw.a945d858.jpg"
+  }
+]);
